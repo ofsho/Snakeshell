b81b59e491b2baddff7d3989509ecc2b8c2ec46e
ofsho <ofekbendavid9@gmail.com>
2020-10-02 00:12:54 +0300

    package edits

--- Setup.py
+++ Setup.py
@@ -1,24 +1,35 @@
-import setuptools
-from setuptools import find_packages, setup
+from distutils.core import setup
 
 with open("README.MD", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="SnakeShell",
-    version="0.0.0.4b2",
-    author="Ofsho",
-    description="Shell script commands in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=[
-        'snakeshell',
-        'snakeshell.package',
-        'snakeshell.utils',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
-    ],
-    install_requires=['distro', 'numba']
+setup(
+  name = 'snakeshell',
+  packages = ['snakeshell', 'snakeshell.package', 'snakeshell.utils'],
+  version = '0.0.0.4c0',
+  license='MIT',
+  description = 'A Python library for all kinds of Shell Commands',
+  author = 'ofshi',
+  author_email = 'spamley101@gmail.com',
+  url = 'https://github.com/ofsho/Snakeshell',
+  download_url = 'https://github.com/ofsho/Snakeshell',
+  keywords = ['SHELL', 'PYTHON', 'LIBRARY', 'TERMINAL'],
+  install_requires=[
+          'validators',
+          'beautifulsoup4',
+          'numba',
+          'distro'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
 )
--- snakeshell/__init__.py
+++ snakeshell/__init__.py
@@ -1,11 +1,1 @@
-import os
-import sys
-import mmap
-
-platform = sys.platform
-
-exists = True
-
-def sample(i):
-    i += 1
-    return i
+from snakeshell import Code, Command
